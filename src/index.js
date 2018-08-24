cb94d2efa9e56449876520103b2afefe5a250e4f
Tony Turetsky <TTuretsky@gmail.com>
2018-08-24 12:07:24 -0400

    up to step 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 3 //Enter your assigned imageId here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -8,7 +8,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const likeButton = document.getElementById("like_button")
+
+  const imageElement = document.getElementById("image")
+
+  const imageName = document.getElementById("name")
+
+  const likeSpan = document.getElementById("likes")
+
+  const ulID = document.getElementById("comments")
+
+
+fetch(imageURL).then( res => res.json() ).then( data => {
+  console.log("this is data:", data)
+
+  likeButton.addEventListener('click', e => addLike(e) )
+
+
+  imageElement.src = data.url
+
+  imageName.innerText = data.name
+
+  likeSpan.innerText = data.like_count
+
+  function addLike(e){
+    data.like_count += 1
+    likeSpan.innerText = data.like_count
+  }
+
+  data.comments.forEach( comment => {
+    ulID.innerHTML += `<li> ${comment.content}`
+  })
 
 
 })
 
+
+})
