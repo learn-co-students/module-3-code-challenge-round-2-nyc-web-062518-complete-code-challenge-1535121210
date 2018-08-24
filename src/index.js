f571a9afc9ac98c1d7b58309aa306f6c9caa8753
Tony Turetsky <TTuretsky@gmail.com>
2018-08-24 12:47:19 -0400

    finished :-)

--- src/index.js
+++ src/index.js
@@ -18,12 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const ulID = document.getElementById("comments")
 
+  const commentForm = document.getElementById("comment_form")
+
+  const submitButton = document.getElementById("submit-button")
+
+  const commentInput = document.getElementById("comment_input")
 
 fetch(imageURL).then( res => res.json() ).then( data => {
   console.log("this is data:", data)
 
-  likeButton.addEventListener('click', e => addLike(e) )
-
+  likeButton.addEventListener('click', e => addLike() )
 
   imageElement.src = data.url
 
@@ -31,17 +35,67 @@ fetch(imageURL).then( res => res.json() ).then( data => {
 
   likeSpan.innerText = data.like_count
 
-  function addLike(e){
+  function addLike(){
     data.like_count += 1
     likeSpan.innerText = data.like_count
+    likePostFetch()
   }
 
   data.comments.forEach( comment => {
     ulID.innerHTML += `<li> ${comment.content}`
   })
 
+  commentForm.addEventListener('click', e=> {
+    if (e.target === submitButton){
+      e.preventDefault()
+
+      let inputVal = commentInput.value
+
+
+      const newLI = document.createElement("li")
+      newLI.innerText = inputVal
+      ulID.appendChild(newLI)
+      commentInput.value = ' '
+      commentPostFetch(inputVal)
+    }
+  })
+
 
 })
+
+
+function commentPostFetch(inputVal){
+
+  const objData = {
+    method: "POST",
+    headers: {'Accept': 'application/json',
+    'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      content: inputVal,
+      image_id: 3
+    }),
+  }
+
+  fetch(commentsURL, objData)
+
+}
+
+
+
+
+
+
+function likePostFetch(){
+  const objData = {
+    method: "POST",
+    headers: {'Accept': 'application/json',
+    'Content-Type': 'application/json'},
+    body: JSON.stringify({id_number: 3})
+    }
+
+  fetch(likeURL, objData)
+    
+}
 
 
 })
